f3d27cb910a5a11401a4b780e7959d10c2f2ea70
Kaese72 <charliegen@hotmail.se>
2025-09-01 01:06:32 +0200

    changed home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="a4-paper">
-    <h2>Welcome to the Blog</h2>
-    <p>This is your first post. More content coming soon!</p>
+    <h2>Welcome to the Dum Bass Blog</h2>
+    <p>This blog is simply my creative outlet where I share my some stupid thoughts, surprising successes, and inevitable failures.</p>
+    <p>Maintained with blood, sweat, tears, but mostly just coffee and GitHub Copilot.</p>
+    <p>Check out the <a href="blogs">posts</a> for blog entries.</p>
   </div>
 </template>
 
